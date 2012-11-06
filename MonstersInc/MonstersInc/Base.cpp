83743aaf0a5187a4522091803ea0ffbd6618be82
Jaime <jaimeaughney@gmail.com>
2012-11-06 21:36:48 +0000

    added base class

---
+++ MonstersInc/MonstersInc/Base.cpp
@@ -0,0 +1,9 @@
+//
+//  Base.cpp
+//  MonstersInc
+//
+//  Created by Jaime Aughney on 06/11/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ MonstersInc/MonstersInc/Base.h
@@ -0,0 +1,14 @@
+//
+//  Base.h
+//  MonstersInc
+//
+//  Created by Jaime Aughney on 06/11/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef MonstersInc_Base_h
+#define MonstersInc_Base_h
+
+
+
+#endif
